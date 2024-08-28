181552f851e4395236f395aeacfb1da76be0e2b7
neels22 <indraneelsarode@gmail.com>
2024-08-28 11:30:32 +0530

    28 aug 24 - errors handling - try except else finally

---
+++ errors and exceptions/try_except_finally.py
@@ -0,0 +1,49 @@
+
+
+# errors arise 
+# we use error handling to let the script continue execution even if we encounter an error
+
+
+# try --block of code to be attempted
+  
+# except -- in case error in try this will execute
+
+# finally -- this code executes no matter what  -- not compulsory 
+
+# many types of errors are their typerros , os errors etc
+
+
+
+try:
+    f = open('testfile','r')
+    f.write('this is written')
+
+except TypeError:
+    print('type error') # for example 10 + '10'
+except OSError:
+    print('you have os error') # because it was opened in r mode 
+except:
+    print('all other errors')
+finally:
+    print('i always run')
+
+
+
+##################################################
+
+
+def ask_for_int():
+
+    while True:
+        try:
+            res = int(input('enter a number'))
+        except:
+            print('oops its not a number')
+            continue
+        else:
+            print('yes thank you')
+            break
+        # finally:
+        #     print('finally ')
+
+ask_for_int()
