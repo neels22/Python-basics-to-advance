963872d5a25329c9473ce4f5f8f41c4ad8aac0ed
neels22 <indraneelsarode@gmail.com>
2024-09-04 13:44:46 +0530

    4 sept 24 - exercise solution

---
+++ Advance python_modules/exercise/exercise.py
@@ -0,0 +1,39 @@
+# https://github.com/Pierian-Data/Complete-Python-3-Bootcamp/blob/master/12-Advanced%20Python%20Modules/08-Advanced-Python-Module-Exercise/08-Advanced-Modules-Exercise-Solutions.ipynb
+
+
+
+import shutil
+shutil.unpack_archive('unzip_me_for_instructions.zip','','zip')
+
+with open('extracted_content/Instructions.txt') as f:
+    content = f.read()
+    print(content)
+
+
+import re
+# pattern = r'\d{3}-\d{3}-\d{4}'
+# test_string = "here is a random number 1231231234 , here is phone number formatted 123-123-1234"
+# re.findall(pattern,test_string)
+
+def search(file,pattern= r'\d{3}-\d{3}-\d{4}'):
+    f = open(file,'r')
+    text = f.read()
+    
+    if re.search(pattern,text):
+        return re.search(pattern,text)
+    else:
+        return ''
+    
+
+
+import os
+results = []
+for folder , sub_folders , files in os.walk(os.getcwd()+"\\extracted_content"):
+    
+    for f in files:
+        full_path = folder+'\\'+f
+         
+        results.append(search(full_path)) 
+for r in results:
+    if r != '':
+        print(r.group())
