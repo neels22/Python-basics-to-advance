def2166b516cf8495df887131ffe4a8f6ecb86a2
neels22 <indraneelsarode@gmail.com>
2024-10-15 22:54:14 +0530

    recursion say digit

--- practice1.py
+++ practice1.py
@@ -20,8 +20,18 @@ def fib(num):
     return fib(num-1) + fib(num-2)
 
 
+def say_digits(num,map):
 
 
+    if num ==0:
+        return 
+
+    digit = num%10
+    num //=10
+
+    say_digits(num,map)
+    
+    print(map[digit])
 
 
 
@@ -37,5 +47,20 @@ def main():
     ans = fib(8)
     print(ans)
 
+    map={
+        1:"one",
+        2:"two",
+        3:"three",
+        4:"four",
+        5:"five",
+        6:"six",
+        7:"seven",
+        8:"eight",
+        9:"nine",
+        0:"zero"
+    }
+
+    ans = say_digits(412,map)
+    print(ans)
 
 main()
